e3222b62cf1da3d4795151102753b380489eb914
Seba <seb.santis@duocuc.cl>
2026-05-12 17:36:59 -0400

    Controller terminado

--- src/main/java/com/Clases/Clases/Controller/ClaseController.java
+++ src/main/java/com/Clases/Clases/Controller/ClaseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/clases")
 @RequiredArgsConstructor
 public class ClaseController {
     private final ClaseService claseService;
